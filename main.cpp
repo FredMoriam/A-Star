773599834957e7d4c69f1a4fed1d0cb55b4ef6cc
FredMoriam <rimandofredrik@gmail.com>
2026-05-17 09:30:31 +0800

    add construction of graph

--- main.cpp
+++ main.cpp
@@ -34,15 +34,18 @@ std::vector<std::vector<std::pair<node, int>>> adjacencyList;
 
 void connect(node base, node other);
 int getNodeDistance(node base, node other);
+void constructGraph();
 
 
 int main() {
+    constructGraph();
+    
     return 0;
 }
 
 void connect(node base, node other) {
     int weight = getNodeDistance(base, other);
-    
+
     for (int i = 0; i < adjacencyList.size(); i++) {
         // Check if the node exists in the adjacency list
         if (adjacencyList[i][0].first.name == base.name) {
@@ -61,4 +64,38 @@ int getNodeDistance(node base, node other) {
     float x = pow((base.x - other.x), 2);
     float y = pow((base.y - other.y), 2);
     return ceil(sqrt(x + y));
+}
+
+// Based on the latest 'Graph-v#.drawio.png'
+void constructGraph() {
+    connect(A, B);
+    connect(A, C);
+    connect(B, A);
+    connect(B, D);
+    connect(B, E);
+    connect(C, A);
+    connect(C, D);
+    connect(C, F);
+    connect(D, B);
+    connect(D, C);
+    connect(D, E);
+    connect(D, F);
+    connect(E, B);
+    connect(E, D);
+    connect(E, G);
+    connect(F, C);
+    connect(F, D);
+    connect(F, G);
+    connect(F, H);
+    connect(G, E);
+    connect(G, F);
+    connect(G, I);
+    connect(H, F);
+    connect(H, I);
+    connect(H, J);
+    connect(I, G);
+    connect(I, H);
+    connect(I, J);
+    connect(J, H);
+    connect(J, I);
 }
